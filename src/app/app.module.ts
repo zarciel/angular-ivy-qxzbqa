b3cf9827a7275b733a3069ae2572777eb31cf03d
zarciel <zarciel@gmail.com>
2020-11-16 15:20:00 +0100

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { UserComponent } from './user/user.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { UserComponent } from "./user/user.component";
+import { NavComponent } from "./nav/nav.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, UserComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, UserComponent, NavComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
